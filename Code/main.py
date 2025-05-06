21f5a2c0589c3e3c92bc3ed4f43d637b4a158021
ratatouy <fuzig2@gmail.com>
2025-05-06 11:44:46 +0200

    start brute forcing

--- Code/main.py
+++ Code/main.py
@@ -4,6 +4,34 @@ from helper import make_regions, make_sanctuaries, make_value_lists
 from functions import *
 from random import randint
 
+def brute_force(path_instance) :
+    '''brute_force toutes les possibilités d'une instance'''
+    max = 0
+    lst_regions, lst_sanctuaries = make_value_lists("../Sujet/Instances_hors_compétition/8_7_a.txt")
+
+    regions = make_regions(lst_regions)
+    sanctuaries = make_sanctuaries(lst_sanctuaries)
+
+    lst = []
+
+    for i in range(len(regions)):
+        lst.append([])
+        for j in range(len(regions)-1):
+            lst[i].append([])
+            for k in range(len(regions)-2):
+                lst[i][j].append([])
+                for l in range(len(regions)-3):
+                    lst[i][j][k].append([])
+                    for m in range(len(regions)-4):
+                        lst[i][j][k][l].append([])
+                        for n in range(len(regions)-5):
+                            lst[i][j][k][l][m].append([])
+                            for o in range(len(regions)-6):
+                                lst[i][j][k][l][m][n].append([])
+                                for p in range(len(regions)-7):
+                                    lst[i][j][k][l][m][n][o] = Board(sanctuaries)
+
+
 if __name__ == '__main__':
 
     lst_regions, lst_sanctuaries = make_value_lists("../Sujet/Instances_hors_compétition/8_7_a.txt")
@@ -24,7 +52,9 @@ if __name__ == '__main__':
     
     print(test.merveilles, test.score)"""
 
-    test = Board(make_sanctuaries(lst_sanctuaries))
+    brute_force("../Sujet/Instances_hors_compétition/8_7_a.txt")
+
+    '''test = Board(make_sanctuaries(lst_sanctuaries))
     cards = make_regions(lst_regions)
     for i in range(8):
         nb = randint(0, len(cards) - 1)
@@ -37,4 +67,4 @@ if __name__ == '__main__':
         print(test.score)
     test.calc_sanctuary_score()
     
-    print(test.merveilles, test.score)
+    print(test.merveilles, test.score)'''
