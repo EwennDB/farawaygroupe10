80a2e0a18852e6ef4a755b4d9f009b2ebccc5568
ratatouy <fuzig2@gmail.com>
2025-05-14 14:50:07 +0200

    sa

--- Code/helper.py
+++ Code/helper.py
@@ -34,7 +34,7 @@ def make_card(nb):
         data = json.load(file)
         args = data[nb]
 
-        return Card(args[0], args[1], args[2], args[3], args[4], functions[args[5]], conditions[args[6]]))
+    return Card(args[0], args[1], args[2], args[3], args[4], functions[args[5]], conditions[args[6]])
 
 def make_sanctuaries(lst) -> list:
     '''créé une liste d'object Carte sanctuaires avec les valeurs données dans la liste'''
