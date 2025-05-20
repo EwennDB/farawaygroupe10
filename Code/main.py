7e4db2de0b2204ca9d66480ba0eda2cd5a99e28a
ratatouy <fuzig2@gmail.com>
2025-05-20 14:46:38 +0200

    t

--- Code/main.py
+++ Code/main.py
@@ -8,25 +8,6 @@ import copy
 
 if __name__ == '__main__':
 
-
-    # lst_regions = [1,27,22,33,47,53,66,67,68]
-    # lst_sanctuaries = [103,108,109,111,118,120,122]
-
-    # #créé les cartes régions et sanctuaires
-    # test = Board(make_sanctuaries(lst_sanctuaries))
-    # cards = make_regions(lst_regions)
-
-    # #révèle les régions
-    # for i in range(8):
-    #     test.place_card(cards.pop(0))
-
-    # #révèle les sanctuaires
-    # for i in range(test.nb_sanc):
-    #     test.add_sanctuary(test.sanctuaire_dispo[0])
-
-    # test.count_all()
-    # print(test.score)
-
     lst_regions, lst_sanctuaries = make_value_lists("../Sujet/Instances_compétition/competition_10.txt")
 
     #créé les cartes régions et sanctuaires
