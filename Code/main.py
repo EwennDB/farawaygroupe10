254443ad3e54d008cceb4223a655e1dd66d289f1
ratatouy <fuzig2@gmail.com>
2025-05-25 21:54:04 +0200

    t pss

--- Code/main.py
+++ Code/main.py
@@ -9,26 +9,3 @@ if __name__ == '__main__':
     filepath = "../Sujet/Instances_compétition/" + filepath
     best = get_best_score(filepath)
     print(best, best.evaluate())
-
-    # for i in range(3,10):
-    #     filepath = f"../Sujet/Instances_compétition/competition_0{str(i)}.txt"
-
-    #     print(f"compétition n°{str(i)} :")
-
-    #     t = time()
-    #     best = get_best_score(filepath)
-    #     print(f"temps : {time()-t}")
-    #     print(f"score : {best.evaluate()}")
-
-    # #     add_sol(filepath, best)
-    
-    # for i in range(0,6):
-    #     filepath = f"../Sujet/Instances_compétition/competition_1{str(i)}.txt"
-
-    #     print(f"compétition n°1{str(i)} :")
-    #     t = time()
-    #     best = get_best_score(filepath)
-    #     print(f"temps : {time()-t}")
-    #     print(f"score : {best.evaluate()}")
-
-    #     add_sol(filepath, best)
